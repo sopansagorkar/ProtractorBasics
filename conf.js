ebca0a7825121f8434beb9354b29c7d5c843b714
sopansagorkar <sagorkars@gmail.com>
2022-03-06 22:41:31 +0530

    Format code

--- conf.js
+++ conf.js
@@ -1,5 +1,8 @@
-exports.config={
+exports.config = {
 
-    seleniumAddress:'http://192.168.29.176:4444/wd/hub',
-    specs:['specs/spec2.js','specs/spec1.js','specs/spec.js']
+    seleniumAddress: 'http://192.168.29.176:4444/wd/hub',
+    specs: ['specs/spec2.js', 'specs/spec1.js', 'specs/spec.js'],
+    capabilities: {
+        browserName: 'chrome'
+    }
 };
--- specs/spec.js
+++ specs/spec.js
@@ -1,9 +1,8 @@
 const { browser, element } = require("protractor")
 
-describe('Enter name Feature',function(){
+describe('Enter name Feature', function () {
 
-    it('should enter name as Sopan',function(){
-
+    it('should enter name as Sopan', function () {
         //driver.get() in Java
         browser.get('https://angularjs.org/');
 
@@ -14,7 +13,7 @@ describe('Enter name Feature',function(){
         browser.driver.sleep(3000);
 
         //Declaring Variable
-        var text=element(by.xpath('/html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
+        var text = element(by.xpath('/html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
 
         //Asserting Text
         expect(text.getText()).toEqual('Hello Sopan!');
@@ -23,12 +22,13 @@ describe('Enter name Feature',function(){
     });
 });
 
-describe('Title Test',function(){
+describe('Title Test', function () {
 
-    it('get title test',function(){
+    it('get title test', function () {
 
         //As google is noy built in AngularJS , we are disabling Angular check for browser
         browser.waitForAngularEnabled(false);
+        browser.ignoreSynchronization = true;
         browser.get('https://www.google.com');
         expect(browser.getTitle()).toEqual('Google');
 
